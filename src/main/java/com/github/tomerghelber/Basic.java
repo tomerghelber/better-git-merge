c8767a62254635537c49a8b61ae0d270eb670978
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-10-27 00:53:32 +0200

    Update Basic.java

--- src/main/java/com/github/tomerghelber/Basic.java
+++ src/main/java/com/github/tomerghelber/Basic.java
@@ -7,8 +7,9 @@ import com.github.tomerghelber.ast.Variable;
  */
 public class Basic {
     public static boolean isRename(Variable first, Variable second) {
-        return first.getIdentifier() == second.getIdentifier();
+        return first.getIdentifier() != second.getIdentifier();
     }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
