5c1e92777fe2928f31e0229aa8ca66b5e771a1e1
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-10-27 00:26:03 +0200

    Update and rename src/test/java/com/github/tomerghelber1 to src/test/java/com/github/tomerghelber/RenameTests.java

---
+++ src/test/java/com/github/tomerghelber/RenameTests.java
@@ -0,0 +1,17 @@
+package com.github.tomerghelber;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.github.tomerghelber.ast.*;
+
+import org.junit.jupiter.api.Test;
+
+class RenameTests {
+    @Test
+    void twoVariable() {
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        bool ret Basic.isRename(a, b);
+        assertEquals(true, ret);
+    }
+}
