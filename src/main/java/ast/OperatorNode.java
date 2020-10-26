b12dd4ef688b0d42feb1208e5c8317a77cfc6400
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2020-10-27 00:15:05 +0200

    Update OperatorNode.java

--- src/main/java/ast/OperatorNode.java
+++ src/main/java/ast/OperatorNode.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class OperatorNode implements ASTNode  {
     private final Variable variable;
-    private final Node value;
+    private final ASTNode value;
     private final Operator operator;
 
     public static enum Operator {
